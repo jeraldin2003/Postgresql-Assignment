602b8ae267ae8d2d8167a1a78e6614082b7c95a0
jeraldin2003 <jeraldin2003@gmail.com>
2026-07-03 17:13:19 +0530

    adds join queries to the sql file

--- postgres.sql
+++ postgres.sql
@@ -1,18 +1,18 @@
-CREATE TABLE Users (
+Create table Users (
      user_id SERIAL PRIMARY KEY,
      name VARCHAR(100) NOT NULL,
      email VARCHAR(100) UNIQUE NOT NULL,
      phone VARCHAR(15),
      address TEXT
  );
- CREATE TABLE Products (
+ Create Table Products (
      product_id SERIAL PRIMARY KEY,
      product_name VARCHAR(100) NOT NULL,
      description TEXT,
      price DECIMAL(10,2) NOT NULL,
      stock INT DEFAULT 0
  );
-CREATE TABLE Orders (
+Create Table Orders (
      order_id SERIAL PRIMARY KEY,
      user_id INT NOT NULL,
      order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -21,7 +21,7 @@ CREATE TABLE Orders (
      FOREIGN KEY (user_id)
      REFERENCES Users(user_id)
  );
- CREATE TABLE Order_Items (
+Create Table Order_Items (
      order_item_id SERIAL PRIMARY KEY,
      order_id INT NOT NULL,
      product_id INT NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE Orders (
      FOREIGN KEY (product_id)
      REFERENCES Products(product_id)
  );
-
+Insert Queries
  Insert into users (user_id, name, email, phone, address) values(
 
     1, 'John Doe', 'john@gmail.com', '1234567890', 'Noida Sec 142'
@@ -81,3 +81,38 @@ Insert into orders (order_id, user_id, order_date, total_amount) values(
 
     3, 2, 2, 1, 500.00
  );
+
+Join Queries
+Lists all orders with customer name
+Select
+    o.order_id,
+    u.name AS customer_name,
+    o.order_date,
+    o.total_amount
+From Orders o
+Join Users u
+On o.user_id = u.user_id;
+Lists all products in every order
+Select
+    o.order_id,
+    p.product_name,
+    oi.quantity,
+    oi.price
+From Order_Items oi
+Join Orders o
+On oi.order_id = o.order_id
+Join Products p
+On oi.product_id = p.product_id
+Order By o.order_id;
+shows total orders placed by each customers
+Select
+    u.user_id,
+    u.name,
+    COUNT(o.order_id) AS total_orders
+From Users u
+Left Join Orders o
+On u.user_id = o.user_id
+Group By
+    u.user_id,
+    u.name
+Order By total_orders desc;
